c9d169034986a51a9d1f8d680a2106b043f5ece2
Serhii Nykyforov <04mailbox@gmail.com>
2020-12-28 22:11:40 +0200

    Make platform move between multiple points

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,31 +2,53 @@
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Transform top;
-    public Transform bottom;
+    public Transform[] path;
     public float speed;
+    public int startPoint;
 
-    private float current;  // от 0 до 1
-    private float dir;
+    private Transform _from;
+    private Transform _to;
+    
+    private float _currentPosition; // from 0 to 1
+    private int _platformDestinationIdx;
+    private int _direction;
 
     void Start()
     {
-        current = 0.0f;
-        dir = 1.0f;
+        _currentPosition = 0.0f;
+        _direction = 1;
+        _platformDestinationIdx = startPoint;
+        _from = path[_platformDestinationIdx];
+        CalcNextPlatformDestinationIdx();
+        _to = path[_platformDestinationIdx];
     }
 
     void Update()
     {
-        current += dir * speed * Time.deltaTime;
-        if (current > 1.0f) {
-            current = 1.0f;
-            dir = -1.0f;
-        } else if (current < 0.0f) {
-            current = 0.0f;
-            dir = 1.0f;
+        _currentPosition += speed * Time.deltaTime;
+        if (_currentPosition > 1.0f)
+        {
+            _currentPosition = 0.0f;
+            _from = _to;
+            CalcNextPlatformDestinationIdx();
+            _to = path[_platformDestinationIdx];
         }
 
-        transform.position = Vector3.Lerp(top.position, bottom.position, current);
+        transform.position = Vector3.Lerp(_from.position, _to.position, _currentPosition);
+    }
+
+    private void CalcNextPlatformDestinationIdx()
+    {
+        if (_platformDestinationIdx >= path.Length - 1)
+        {
+            _direction = -1;
+        }
+
+        if (_platformDestinationIdx <= 0)
+        {
+            _direction = 1;
+        }
+
+        _platformDestinationIdx += _direction;
     }
 }
-
